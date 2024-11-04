a2624d702bbc83d642d09c1c470f2cd625587eef
Kononiuk <hmasteraaa@gmail.com>
2024-11-04 11:03:16 +0200

    added leaf animation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import Lottie from "lottie-react";
+import * as animationData from './assets/leaf.json';
+
+const defaultOptions = {
+   loop: false,
+   //  autoplay: true,
+   animationData: animationData
+   //  rendererSettings: {
+   //     preserveAspectRatio: 'xMidYMid slice'
+   //  }
+};
+
+const interactivity = {
+   mode: 'scroll',
+   actions: [
+      {
+         visibility: [0.01, 0.45],
+         type: 'seek',
+         frames: [-29, 100]
+
+      }
+   ]
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className='App'>
+         <Lottie
+            interactivity={interactivity}
+            animationData={animationData}
+            // options={defaultOptions}
+            loop={false}
+         />
+         {/* <img
+            loading='lazy'
+            decoding='async'
+            width='114'
+            height='114'
+            src='wp-content/uploads/2024/10/leaf1.gif'
+            class='center-image'
+            alt=''
+         /> */}
+      </div>
+   );
 }
 
 export default App;
